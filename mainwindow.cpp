e0752ead74690f5fec76f410fbbb4931bce1f033
koldbyte <bhaskar.divya007@gmail.com>
2014-04-08 11:15:56 +0530

    Added code for file open and save

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -12,3 +12,63 @@ MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void MainWindow::on_actionOpen_triggered(){
+    QString fileName = QFileDialog::getOpenFileName(this,
+             tr("Open Source Code"), "",
+             tr("Source Code (*.sic);Text File(*.txt);;All Files (*)"));
+    if (fileName.isEmpty())
+             return;
+         else {
+             QFile file(fileName);
+
+             if (!file.open(QIODevice::ReadOnly)) {
+                 QMessageBox::information(this, tr("Unable to open file"),
+                     file.errorString());
+                 return;
+             }
+
+             QTextStream stream(&file);
+             ui->SourceCodeTextEdit->clear();
+             QString in;
+             in = stream.readAll();
+             //stream >> in;
+             ui->SourceCodeTextEdit->setPlainText(in);
+        }
+}
+
+void MainWindow::on_actionSave_to_File_triggered(){
+    QString fileName = QFileDialog::getSaveFileName(this,
+             tr("Save Source Code"), "",
+             tr("Source Code (*.sic);Text File(*.txt);;All Files (*)"));
+    if (fileName.isEmpty())
+             return;
+         else {
+             QFile file(fileName);
+             if (!file.open(QIODevice::WriteOnly)) {
+                 QMessageBox::information(this, tr("Unable to Save file"),
+                     file.errorString());
+                 return;
+             }
+             QTextStream stream(&file);
+             stream << ui->SourceCodeTextEdit->toPlainText();
+        }
+}
+
+void MainWindow::on_actionSave_Output_triggered(){
+    QString fileName = QFileDialog::getSaveFileName(this,
+             tr("Save Object Code"), "",
+             tr("Source Code (*.obj);Text File(*.txt);;All Files (*)"));
+    if (fileName.isEmpty())
+             return;
+         else {
+             QFile file(fileName);
+             if (!file.open(QIODevice::WriteOnly)) {
+                 QMessageBox::information(this, tr("Unable to Save file"),
+                     file.errorString());
+                 return;
+             }
+             QTextStream stream(&file);
+             stream << ui->ObjectCodeText->toPlainText();
+        }
+}
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,12 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include "QPlainTextEdit"
+#include "QTextEdit"
+#include "Gui/codeeditor.h"
+#include "QFileDialog"
+#include "QMessageBox"
+#include "QTextStream"
 
 namespace Ui {
 class MainWindow;
@@ -14,6 +20,13 @@ class MainWindow : public QMainWindow
 public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
+
+private slots:
+    void on_actionOpen_triggered();
+
+    void on_actionSave_to_File_triggered();
+
+    void on_actionSave_Output_triggered();
 
 private:
     Ui::MainWindow *ui;
