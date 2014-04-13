02fa30f6a5f45bee01783a41bd3d1a39cb96d645
koldbyte <bhaskar.divya007@gmail.com>
2014-04-13 12:26:36 +0530

    Completed literal data type

--- Data/literal.cpp
+++ Data/literal.cpp
@@ -1,5 +1,45 @@
 #include "literal.h"
+#include "QIntegerForSize"
+Literal::Literal(){
+    ltorgDump = -1;
+    location = -1;
+}
 
-Literal::Literal()
-{
+QString Literal::getValue() {
+    QString ret = QString::number(value,16);
+    //TODO Verify
+    //QString ret = Integer.toHexString(value);
+    while(ret.length() < length * 2) {
+        ret = '0' + ret;
+    }
+    return ret;
+    //return Utils.expand(value, length * 2);
+}
+
+void Literal::setValue(int value) {
+    this->value = value;
+}
+
+int Literal::getLength() {
+    return length;
+}
+
+void Literal::setLength(int length) {
+    this->length = length;
+}
+
+int Literal::getLocation() {
+    return location;
+}
+
+void Literal::setLocation(int location) {
+    this->location = location;
+}
+
+int Literal::getLtorgDump() {
+    return ltorgDump;
+}
+
+void Literal::setLtorgDump(int ltorgDump) {
+    this->ltorgDump = ltorgDump;
 }
--- Data/literal.h
+++ Data/literal.h
@@ -1,10 +1,24 @@
 #ifndef LITERAL_H
 #define LITERAL_H
-
+#include "QString"
 class Literal
 {
+    int value;
+    int length;
+    int location;
+    int ltorgDump;
 public:
     Literal();
+
+    QString getValue() ;
+    void setValue(int value) ;
+    int getLength();
+    void setLength(int length);
+    int getLocation() ;
+    void setLocation(int location) ;
+    int getLtorgDump() ;
+    void setLtorgDump(int ltorgDump) ;
+
 };
 
 #endif // LITERAL_H
